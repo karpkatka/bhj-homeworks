8b01b730076bf0e02d2c19c926a7cc4501c083dc
Ekaterina Bakaeva <bakaeva.es@gmail.com>
2023-05-08 21:39:48 +0300

    Mole-game fixed

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,4 +1,6 @@
 let getHole = (index) => document.getElementById(`hole${index}`); 
+const textChange = document.querySelector('.status');
+textChange.innerHTML = 'Поймано кротов: <span id="dead">0</span><br> Убежало: <span id="lost">0</span><br>';
 const winStatus = document.getElementById('dead');
 const loseStatus = document.getElementById('lost');
 function clear() {
@@ -11,11 +13,11 @@ for (let i = 1; i < 10; i++){
         lost = +loseStatus.textContent;
     if (getHole(i).classList.contains('hole_has-mole') && value < 9) { 
         winStatus.textContent = value +1; 
-    } else if (getHole(i).classList.contains('hole_has-mole') && value === 10) {
+    } else if (getHole(i).classList.contains('hole_has-mole') && value === 9) {
+        winStatus.textContent = value +1; 
         alert ("Вы поймали кротов!");
         clear();
     } else if (lost < 4) {
-        
         loseStatus.textContent = lost + 1;
     } else {
         alert ("Кроты убежали!");
