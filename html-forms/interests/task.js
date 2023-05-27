c7b41bc9f6659242b4563f989f559d38344d323f
Ekaterina Bakaeva <bakaeva.es@gmail.com>
2023-05-27 16:43:32 +0300

    Interests somewhat done

---
+++ html-forms/interests/task.js
@@ -0,0 +1,14 @@
+const interestCheck = Array.from(document.querySelectorAll(".interest__check"));
+
+interestCheck.forEach (item => {
+    const interestChild = Array.from(item.closest(".interest").querySelectorAll(".interest__check"));
+
+    item.addEventListener("change", (event) => {
+    if (item.checked) {
+        interestChild.forEach(element => element.checked = true);
+    } else {
+        interestChild.forEach(element => element.checked = false);
+    }
+    });
+}
+);
